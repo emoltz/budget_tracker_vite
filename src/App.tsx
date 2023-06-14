e1c8ebadca86248b82dcd5e3d5ee45ce0ba7d526
emoltz <es5541@nyu.edu>
2023-06-14 11:42:44 -0400

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './login/LoginPage';
+import HomePage from "./HomePage.tsx";
 function App() {
 
-  return (
-    <>
-     Hello
-    </>
-  )
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path={"/"} element={<HomePage/>}/>
+                </Routes>
+            </Router>
+
+
+        </>
+)
 }
 
 export default App
---
+++ src/HomePage.tsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+export default function HomePage() {
+    return (
+        <>
+            <div>
+
+                Home page
+            </div>
+            <Link to={"/login"}>Login</Link>
+        </>
+    )
+}
