6796ea6785a7e6059c774b32268b110c89222ab3
emoltz <es5541@nyu.edu>
2023-06-14 12:03:39 -0400

    basic setup

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,13 +1,23 @@
 import {Link} from "react-router-dom";
+import {Button} from '@mantine/core';
 
 export default function HomePage() {
     return (
         <>
             <div>
-
                 Home page
             </div>
-            <Link to={"/login"}>Login</Link>
+
+
+            <Link
+                to={"/login"}
+            >
+                <Button>
+
+                    Login
+                </Button>
+            </Link>
+
         </>
     )
 }
